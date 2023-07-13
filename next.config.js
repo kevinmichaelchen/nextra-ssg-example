a4ba107e5f1e935d2ec01c28099d61cdd154b340
Kevin Chen <kevinmichaelchen@gmail.com>
2023-07-13 10:01:06 -0400

    fix: Turn off image optimization in Production

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ const withNextra = require("nextra")({
   latex: true,
 });
 
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = withNextra({
   output: "export",
   transpilePackages: ["react-tweet"],
   images: {
+    unoptimized: isProduction,
     remotePatterns: [
       { protocol: "https", hostname: "pbs.twimg.com" },
       { protocol: "https", hostname: "abs.twimg.com" },
